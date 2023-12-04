3942ab99d09a5ee5ea5732dc79cef32eaa1c50e0
Whilmar Bitoco <whlmrbitoco@gmail.com>
2023-12-04 14:36:09 +0800

    added create function in cotroller

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Project::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'github' => 'required',
+            'image' => 'required',
+            'languages' => 'required'
+        ]);
+        return Project::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Models\Project;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/project', function (){
-    return Project::all();
-});
+Route::get('/project', [ProjectController::class, 'index']);
+Route::post('/project', [ProjectController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
